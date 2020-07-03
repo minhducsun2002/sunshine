c4a4ff2bbb9c53d109d28caf488911d641891c83
Minh Duc Vo <minhducsun2002@gmail.com>
2020-07-04 01:15:58 +0700

    Add `manga`

---
+++ src/Commands/Weeaboo/MAL.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using sunshine.Services;
+using sunshine.Classes;
+
+namespace sunshine.Commands
+{
+    namespace MAL
+    {
+        public class MAL : CommandModuleBase
+        {
+            MAL() { name = "MAL"; }
+
+            public MyAnimeList mal { get; set; }
+
+            [Command("anime")]
+            [Category("Weebs")]
+            public async Task anime([Remainder] string query = "")
+            {
+                var m = Context.Message;
+                var err = new EmbedBuilder() { }.WithColor(Color.Red);
+                if (query == null || query.Length == 0)
+                {
+                    await ReplyAsync(
+                        null, false,
+                        err.WithDescription($"{m.Author.Mention}, I see nothing to search about. :frowning:").Build()
+                    );
+                    return;
+                };
+                var result = await mal.anime(query);
+                if (result.Count < 1) {
+                    await ReplyAsync($"Apologies, {m.Author.Mention}, couldn't find anything that matched.");
+                    return;
+                };
+                var id = result[0].mal_id;
+                var _ = await mal.anime(id);
+                await ReplyAsync(
+                    null, false,
+                    new EmbedBuilder()
+                    {
+                        Title = _.title,
+                        Url = _.url,
+                        ImageUrl = _.image_url,
+                        Description = (
+                            _.synopsis.Length > 2000 ? _.synopsis.Substring(0, 2000) + "..." : _.synopsis
+                        )
+                    } 
+                        .AddField(
+                            "Titles",
+                            $"**Japanese** : {_.title_japanese}"
+                            + (_.title_english.Length > 0 ? $"\n**English** : {_.title_english}" : "")
+                            + (_.title_synonyms.Length > 0 ? $"\n**Alternatives** : {String.Join(", ", _.title_synonyms)}" : "")
+                        )
+                        .AddField(
+                            "General information",
+                            $"**Type** : {_.type}"
+                            + $"\n**Source** : {_.source}"
+                            + (_.episodes == null ? "" : $"\n**Episode** : {_.episodes}")
+                            + $"\n**Premiere** : {_.premiered}"
+                            + $"\n**Duration** : {_.duration}"
+                            + $"\n**Rating** : {_.rating}"
+                            + $"\n{(_.airing ? "**Currently airing**" : $"**Aired** : {_.aired.@string}")}"
+                            + $"\n**Genre** : {String.Join(", ", _.genres.Select(g => g.name).ToArray())}"
+                            + $"\n**Studio** : {String.Join(", ", _.studios.Select(g => g.name).ToArray())}"
+                        )
+                        .Build()
+                );
+
+            }
+
+            [Command("manga")]
+            [Category("Weebs")]            
+            public async Task manga([Remainder] string query = "")
+            {
+                var m = Context.Message;
+                var err = new EmbedBuilder() { }.WithColor(Color.Red);
+                if (query == null || query.Length == 0)
+                {
+                    await ReplyAsync(
+                        null, false,
+                        err.WithDescription($"{m.Author.Mention}, I see nothing to search about. :frowning:").Build()
+                    );
+                    return;
+                };
+                var result = await mal.manga(query);
+                if (result.Count < 1) {
+                    await ReplyAsync($"Apologies, {m.Author.Mention}, couldn't find anything that matched.");
+                    return;
+                };
+                var id = result[0].mal_id;
+                var _ = await mal.manga(id);
+                await ReplyAsync(
+                    null, false,
+                    new EmbedBuilder()
+                    {
+                        Title = _.title,
+                        Url = _.url,
+                        ImageUrl = _.image_url,
+                        Description = (
+                            _.synopsis.Length > 2000 ? _.synopsis.Substring(0, 2000) + "..." : _.synopsis
+                        )
+                    } 
+                        .AddField(
+                            "Titles",
+                            $"**Japanese** : {_.title_japanese}"
+                            + (_.title_english.Length > 0 ? $"\n**English** : {_.title_english}" : "")
+                            + (_.title_synonyms.Length > 0 ? $"\n**Alternatives** : {string.Join(", ", _.title_synonyms)}" : "")
+                        )
+                        .AddField(
+                            "General information",
+                            $"**Type** : {_.type}"
+                            + (_.publishing
+                                ? "\nCurrently publishing"
+                                : $"**\nPublished** : {_.published.@string}")
+                            + (_.volumes == null ? "" : $"\n**Volumes** : {_.volumes}")
+                            + (_.chapters == null ? "" : $"\n**Chapters** : {_.chapters}")
+                            + $"\n**Score** : {_.score}"
+                            + $"\n**Genre** : {string.Join(", ", _.genres.Select(g => $"[{g.name}]({g.url})").ToArray())}"
+                            + $"\n**Author** : {string.Join(", ", _.authors.Select(g => $"[{g.name}]({g.url})").ToArray())}"
+                        )
+                        .Build()
+                );
+
+            }
+        }
+    }
+}
--- src/Services/MyAnimeList.cs
+++ src/Services/MyAnimeList.cs
@@ -10,11 +10,12 @@ namespace sunshine.Services
 {
     namespace MyAnimeListResults
     {
+        // Anime
         public class Anime
         {
             [JsonProperty(NullValueHandling = NullValueHandling.Include)]
-            public Int64 mal_id;
-            public Int64? episodes;
+            public long mal_id;
+            public long? episodes;
             public string url, image_url, title, synopsis, type;
             public bool airing;
             public float score;
@@ -26,7 +27,7 @@ namespace sunshine.Services
             public string title_english, title_japanese, status, duration, rating, premiered, source;
             public string[] title_synonyms;
             public AnimeAirings aired;
-            public AnimeGenre[] genres;
+            public MALObject[] genres;
             public AnimeStudio[] studios;
         }
 
@@ -37,25 +38,81 @@ namespace sunshine.Services
             [JsonProperty(PropertyName = "string")]
             public string @string;
         }
-        public struct AnimeGenre { public Int64 mal_id; public string type, name, url; }
+        public struct MALObject { public long mal_id; public string type, name, url; }
         public struct AnimeStudio { public string type, name, url; }
+
+        // manga
+        public class Manga
+        {
+            // we only need this tbh
+            public long mal_id;
+        }
+        public class MangaDetailed
+        {
+            [JsonProperty(NullValueHandling = NullValueHandling.Include)]
+            public long mal_id, rank;
+            public long? volumes, chapters;
+            public string url, image_url, title, title_english, title_japanese,
+                status, synopsis, type;
+            public MALObject[] genres, authors;
+            public string[] title_synonyms;
+            public MangaPublished published;
+            public bool publishing;
+            public float score;
+        }
+
+        public class MangaPublished
+        {
+            public string from, to;
+            [JsonProperty(PropertyName = "string")]
+            public string @string;
+        }
+
     }
     public class MyAnimeList
     {
-        private string baseURL = "https://api.jikan.moe/v3";
+        private readonly string baseURL = "https://api.jikan.moe/v3";
         private readonly HttpClient client = new HttpClient();
+
+        private async Task<string> _base_search(string obj, string q)
+        {
+            var __ = HttpUtility.UrlEncode(q);
+            return await client.GetStringAsync($"{baseURL}/search/{obj}?q={__}");
+        }
+
+        private async Task<string> _base_info(string obj, long i)
+        {
+            return await client.GetStringAsync($"{baseURL}/{obj}/{i}");
+        }
+
         public async Task<List<MyAnimeListResults.Anime>> anime(string _)
         {
-            var __ = HttpUtility.UrlEncode(_);
-            var response = await client.GetStringAsync($"{baseURL}/search/anime?q={__}");
-            return ((JObject)JsonConvert.DeserializeObject(response))["results"]
+            return ((JObject)JsonConvert.DeserializeObject(
+                await _base_search("anime", _)
+            ))["results"]
                 .ToObject<List<MyAnimeListResults.Anime>>();
         }
 
-        public async Task<MyAnimeListResults.AnimeDetailed> anime(Int64 _)
+        public async Task<MyAnimeListResults.AnimeDetailed> anime(long _)
         {
-            var response = await client.GetStringAsync($"{baseURL}/anime/{_}");
-            return ((JObject)JsonConvert.DeserializeObject(response)).ToObject<MyAnimeListResults.AnimeDetailed>();
+            return ((JObject)JsonConvert.DeserializeObject(
+                await _base_info("anime", _)
+            )).ToObject<MyAnimeListResults.AnimeDetailed>();
+        }
+
+        public async Task<List<MyAnimeListResults.Manga>> manga(string _)
+        {
+            return ((JObject)JsonConvert.DeserializeObject(
+                await _base_search("manga", _)
+            ))["results"]
+                .ToObject<List<MyAnimeListResults.Manga>>();
+        }
+
+        public async Task<MyAnimeListResults.MangaDetailed> manga(long _)
+        {
+            return ((JObject)JsonConvert.DeserializeObject(
+                await _base_info("manga", _)
+            )).ToObject<MyAnimeListResults.MangaDetailed>();
         }
     }
 }
