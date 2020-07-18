d9389208620ea9aa4b923418403dba9f82fb5cae
Minh Duc Vo <minhducsun2002@gmail.com>
2020-07-18 15:50:40 +0700

    Restructure src folder

--- Commands/Information/Avatar.cs
+++ Commands/Information/Avatar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -6,14 +7,29 @@ using sunshine.Classes;
 
 namespace sunshine.Commands
 {
+    public class CustomUserReader<T> : UserTypeReader<T>
+        where T : class, IUser
+    {
+        public CustomUserReader() : base() {}
+        public override Task<TypeReaderResult> ReadAsync(ICommandContext c, string i, IServiceProvider s)
+        {
+            var _ = MentionUtils.TryParseUser(i, out var userId);
+            c.Client.GetUserAsync(userId, CacheMode.AllowDownload).Result.GetOrCreateDMChannelAsync();
+            return base.ReadAsync(c, i, s);
+        }
+    }
+
     public class Avatar : CommandModuleBase
     {
         Avatar() { name = "avatar"; }
 
         [Command("avatar")]
         [Category("Moderation")]
-        public async Task avatar(SocketUser user = null)
+        public async Task avatar(
+            [OverrideTypeReader(typeof (CustomUserReader<IUser>))] IUser user = null
+        )
         {
+            
             if (user == null) user = Context.User;
             await Context.Message.Channel.SendMessageAsync(
                 "", false,
--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1.201-alpine3.11 as build
 WORKDIR /app
 
-COPY src/ src/
-RUN dotnet publish -c release --runtime linux-musl-x64 --self-contained -o dist/ src/
+COPY . .
+RUN dotnet publish -c release --runtime linux-musl-x64 --self-contained -o dist/ 
 
 FROM mcr.microsoft.com/dotnet/core/runtime-deps:3.1-alpine3.11
 COPY --from=build /app/dist /app
