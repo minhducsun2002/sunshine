6ed82eabcea693304348994b3f902064a1d058bd
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-07-21 09:04:02 +0700

    Add schema for manga

---
+++ MyAnimeList.Schema.Manga.cs
@@ -0,0 +1,39 @@
+#nullable enable
+using System;
+using Newtonsoft.Json;
+
+namespace sunshine
+{
+    internal class Manga : MyAnimeListBaseObject
+    {
+        [JsonProperty("image_url")] public string ImageUrl = "";
+        [JsonProperty("publishing")] internal bool Publishing;
+        [JsonProperty("type")] public string Type = "";
+        [JsonProperty("volumes")] internal long? Volumes;
+        [JsonProperty("chapters")] internal long? Chapters;
+        [JsonProperty("score")] internal float? Score;
+        [JsonProperty("start_date")] public string StartDate = "";
+        [JsonProperty("end_date")] public string? EndDate;
+        [JsonProperty("synopsis")] public string Synopsis = "";
+        [JsonProperty("title")] public string Title = "";
+    }
+    
+    internal class MangaDetailed : Manga
+    {
+        [JsonProperty("rank", NullValueHandling = NullValueHandling.Ignore)] internal long Rank;
+        [JsonProperty("title_english")] public string? TitleEnglish;
+        [JsonProperty("title_japanese")] public string? TitleJapanese;
+        [JsonProperty("status")] public string Status = "";
+        [JsonProperty("genres")] public MyAnimeListObject[] Genres = Array.Empty<MyAnimeListObject>();
+        [JsonProperty("authors")] public MyAnimeListObject[] Authors = Array.Empty<MyAnimeListObject>();
+        [JsonProperty("title_synonyms")] internal string[]? TitleSynonyms;
+        [JsonProperty("published")] internal MangaPublished Published;
+    }
+
+    internal class MangaPublished
+    {
+        [JsonProperty("from")] internal string From = "";
+        [JsonProperty("to")] internal string To = "";
+        [JsonProperty("string")] internal string Stringified = "";
+    }
+}
